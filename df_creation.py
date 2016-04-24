1051c50b5769f38a7a07472eb1a5d4e67c6da3ed
xliu <xl3ma@virginia.edu>
2016-04-24 12:35:21 -0400

    Update df_creation.py

--- df_creation.py
+++ df_creation.py
@@ -63,9 +63,11 @@ def orderedPair(pair):
 def addPair(pair,edges,adjList,item):
     pair = orderedPair(pair)
     v1,v2 = pair
+    
+    if pair not in edges:
+        adjList[v1].append(v2)
+        adjList[v2].append(v1)
     edges[pair].append(item)
-    adjList[v1].append(v2)
-    adjList[v2].append(v1)
 
 
 def findCountDevs(df):
@@ -73,6 +75,81 @@ def findCountDevs(df):
     t=t.apply(np.sum,axis=1).to_frame()
     t.columns = ['count']
     return t
+
+
+
+
+def socialLinks(df):
+    #df = df.transpose()
+    edges = collections.defaultdict(list) #count cocurrence
+    adjList = collections.defaultdict(list)
+    for i in (df.index):
+        row = df.loc[i]
+        item = i
+        row = row[row>0]
+        indices = row.index
+        if len(indices)>=2:
+            
+            for pair in list(itertools.combinations(indices, 2)):
+                addPair(pair,edges,adjList,item)
+
+
+    #df.apply(socialLink_row,axis=0)
+    return edges,adjList
+
+def user_feature(df,title):
+    t = df.groupby('commit_name')['language'].all().reset_index()
+    t.columns = ['commit_name','language']
+    t.pivot_table(rows='commit_name',cols='language',aggfunc=len,fill_value=0)
+    t.to_csv(title+'_user_languages_user_feautre.csv')
+
+def item_feature(colVal,df,title):
+    #language
+    t = df.groupby(colVal)['language'].all().reset_index()
+    t.columns = [colVal,'language']
+    t.pivot_table(rows=colVal,cols='language',aggfunc=len,fill_value=0)
+    t.to_csv(title+'_'+colVal+'_languages_item_feature.csv')
+
+    #count_devs
+    t = df.groupby(colVal)['commit_name'].count().reset_index()
+    t.columns = [colVal,'counts']
+    t.to_csv(title+'_'+colVal+'_num_commit_name_item_feature.csv')
+    
+    #deleted lines
+    t = df.groupby(colVal)['deleted_lines'].sum().reset_index()
+    t.to_csv(title+'_'+colVal+'_deleted_lines_item_feature.csv')
+    
+    #added lines
+    t = df.groupby(colVal)['added_lines'].sum().reset_index()
+    t.to_csv(title+'_'+colVal+'_added_lines_item_feature.csv')
+    #bugs
+    t = df.groupby(colVal)['is_bug'].sum().reset_index()
+    t.to_csv(title+'_'+colVal+'_num_bugs_item_feature.csv')
+    #counts
+    t = df.groupby(colVal)['is_bug'].count().reset_index()
+    t.columns = [colVal,'counts']
+    t.to_csv(title+'_'+colVal+'count_feature.csv')
+
+
+
+
+def calcSocialLink(adjList,edges,df_item,dev_item_time,colVal):
+    social_links = collections.defaultdict(int)
+    for k,v in adjList.items():
+        for i in v:
+            pair = orderedPair((k,i))
+            for item in edges[pair]:
+                
+                time_k = dev_item_time[((dev_item_time['commit_name']==k)&(dev_item_time[colVal]==item))]['commit_date'].values
+
+                if time_k:
+                    time_k = time_k[0]
+                    n_commiters = df_item[df_item['commit_date']<time_k]['commit_name'].unique()
+                    if len(n_commiters)>0:
+                        social_links[(k,item)]+=1.0/len(n_commiters)
+    return social_links
+
+
 
 def createDataFrames(threshold,names,dataframe_list):
     for n,df in zip(names,dataframe_list):
@@ -86,7 +163,9 @@ def createDataFrames(threshold,names,dataframe_list):
         project_df=itemFreq_filter(project_df,threshold,n+'_project')
 
         df_file_ = df.loc[df['file_name'].isin(file_df.columns)]
-        df_proj_ = df.loc[df['file_name'].isin(project_df.columns)]
+        df_file_.to_csv(n+'commit_history_dev_15_count_file'+str(threshold)+'.csv')
+        df_proj_ = df.loc[df['project'].isin(project_df.columns)]
+        df_proj_.to_csv(n+'commit_history_dev_15_count_project'+str(threshold)+'.csv')
 
         #user-item first time
         user_item_file = df_file_.sort(['commit_date']).groupby(['commit_name','file_name'])['commit_date'].first().to_frame().reset_index()
@@ -94,20 +173,30 @@ def createDataFrames(threshold,names,dataframe_list):
         user_item_project = df_proj_.sort(['commit_date']).groupby(['commit_name','project'])['commit_date'].first().to_frame().reset_index()
         user_item_project.to_csv(n+'_user_project_time.csv')
 
+        #features
+        user_feature(df_file_,n+'_file_')
+        user_feature(df_proj_,n+'_project_')
+        item_feature('file_name',df_file_,n)
+        item_feature('project',df_proj_,n)
+        
+        
 
         findCountDevs(file_df).to_csv('file_name_'+n+'dev_count.csv')
         findCountDevs(project_df).to_csv('project'+n+'dev_count.csv')
         
-        edges,adjList = socialLinks(file_df)
+        edges,adjList = socialLinks(file_df.transpose())
         cPickle.dump(edges,open(n+'_edges_'+'file'+'.p','w'))
         cPickle.dump(adjList,open(n+'_adjList_'+'file'+'.p','w'))
+        social_links=calcSocialLink(adjList,edges,df_file_,user_item_file,"file_name")
+        cPickle.dump(social_links,open(n+'_social_links_'+'file'+'.p','w'))
 
 
-        edges,adjList = socialLinks(project_df)
+        edges,adjList = socialLinks(project_df.transpose())
         cPickle.dump(edges,open(n+'_edges_'+'project'+'.p','w'))
         cPickle.dump(adjList,open(n+'_adjList_'+'project'+'.p','w'))
-        
-        
+        social_links=calcSocialLink(adjList,edges,df_proj_,user_item_project,"project")
+        cPickle.dump(social_links,open(n+'_social_links_'+'project'+'.p','w'))
+        print social_links
         #sumUsers_filter(file_df,threshold,n+'_file')
         #sumUsers_filter(project_df,threshold,n+'_project')
 
@@ -117,51 +206,16 @@ def createDataFrames(threshold,names,dataframe_list):
 
 
 
-
-def socialLinks(df):
-    edges = collections.defaultdict(list) #count cocurrence
-    adjList = collections.defaultdict(list)
-    for i in (df.index):
-        row = df.loc[i]
-        item = row[0]
-        row = row[row>0]
-        
-        indices = row.index[1:]
-        if len(indices)>1:
-            for pair in list(itertools.combinations(indices, 2)):
-                addPair(pair,edges,adjList,item)
+def dataframe_create(filename):
+    df =pd.read_csv(filename)
+    df = df.ix[:,xrange(3,15)]
+    columns = df.columns
 
 
-    #df.apply(socialLink_row,axis=0)
-    return edges,adjList
+    threshold = 5
+    train_df = filterbyTime(df,'1998-08-08','2013-01-01')
+    test_df = filterbyTime(df,'2013-01-01','2015-09-01')
+    names = ['train','test']
+    createDataFrames(threshold,names,[train_df,test_df])
 
-
-
-def calculateSocialLink(adjList,edges,df_item,dev_item_time,colVal,title):
-    social_links = collections.default(int)
-    for k,v in enumerate(adjList):
-        
-        for i in v:
-            pair = orderedPair((k,i))
-            item = edges[i]
-            time_k = dev_item_time[(dev_item_time['commit_name']==k)&(dev_item_time[colVal]==item)]['commit_date'].values[0]
-            n_commiters = df_item[df_item['commit_date'<time_k]]['commit_name'].unique()
-            social_links[(k,item)]+=1.0/n_commiters
-    return social_links
-
-
-
-df =pd.read_csv('commit_history_dev_15.csv')
-df = df.ix[:,xrange(4,15)]
-columns = df.columns
-
-
-threshold = 5
-train_df = filterbyTime(df,'1998-08-08','2013-01-01')
-test_df = filterbyTime(df,'2013-01-01','2015-09-01')
-names = ['train','test']
-createDataFrames(threshold,names,[train_df,test_df])
-
-
-
-#df = pd.read_csv('train_project_user_matrix_5.csv')
+dataframe_create('commit_history_dev_15.csv')
