dc1b8ee0416b7f6bad97a10d2b2198f347915d87
xliu <xl3ma@virginia.edu>
2016-04-26 00:27:11 -0400

    Update log_file_extraction.py

--- log_file_extraction.py
+++ log_file_extraction.py
@@ -8,6 +8,7 @@ import sys
 def sampleDevs(dir_,file_,k):
     df = pd.read_csv(dir_+file_)
     df.columns = ['person_id','name','email']
+    df = df.loc[df['author_name'].isin(author_list)]
     indices = random.sample(df.index,k)
     return df.ix[indices]['name'].values
 
@@ -18,16 +19,22 @@ def extractLogFiles(df,author_list):
     return res
 
 
-def filterByCount(df,threshold):
-    df1 = df.groupby('author_name')['project'].nunique()
+def filterByCount(df,colVal,threshold):
+    df1 = df.groupby(colVal)['project'].nunique()
     authors1 = df1[df1>=threshold].index
     
-    df1 = df.groupby('author_name')['file_name'].nunique()
+    df1 = df.groupby(colVal)['file_name'].nunique()
     authors2 = df1[df1>=threshold].index
     authors = authors2.intersection(authors1)
     return extractLogFiles(df,authors)
 
 
+def extractDF(df,proj_threshold):
+    t = df.groupby(['project'])['commit_name'].count()
+    proj_index = t[((t>=proj_threshold)&(t<=proj_threshold*3))].index
+    #print proj_index
+    commit_names =proj_index
+    return df.loc[df['project'].isin(commit_names)]
 
 def aggregateLogs():
     k = 40
@@ -37,19 +44,32 @@ def aggregateLogs():
     dir_ = '/bigtemp/dSar/dl_githubdownloadedprojects/'
     file1 = 'githubdownloadedprojects.commit_history_new.csv'
     file2 = 'githubdownloadedprojects.user_map.csv'
-    author_list = sampleDevs(dir_,file2,k)
+    #author_list = sampleDevs(dir_,file2,k)
     df_chunks  = pd.read_csv(dir_+file1,chunksize = 1000000)
     df_final = pd.DataFrame()
+    proj_threshold = 300
+    commit_threshold = 50
     for i,df in enumerate(df_chunks):
         print i
-        res = extractLogFiles(df,author_list)
+        #res = extractLogFiles(df,author_list)
+        res = extractDF(df,proj_threshold)
+        
         df_final = pd.concat([df_final,res])
+        print len(df_final)
     
     
-    df_final = filterByCount(df_final,5)
-    df_final.to_csv("commit_history_dev_"+str(k)+".csv")
+    
+    df_final = filterByCount(df_final,'commit_name',commit_threshold)
+    df_final = filterByCount(df_final,'commit_name',commit_threshold)
+    #df_final = df_final.ix[:,xrange(4,15)]
+    df_final.to_csv("commit_history_dev_threshold_"+str(proj_threshold)+"_"+str(commit_threshold)+".csv")
+
     print 'done'
 
-aggregateLogs()
+def main():
+    aggregateLogs()
+
+if __name__ == '__main__':
+    main()
 
 
